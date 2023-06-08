325c3ac07e8904718a6510a1a7ee6fc7c88df9a3
Alek23k <alemank23k@gmail.com>
2023-06-08 17:07:00 -0300

    last commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import React from "react";
+import styles from "./navbar.module.css";
 
 const Navbar = () => {
   const links = [
@@ -38,17 +39,24 @@ const Navbar = () => {
   ];
 
   return (
-    <div>
-      <Link href="/">alemia</Link>
+    <div className={styles.container}>
+      <Link href="/" className={styles.logo}>
+        Ale
+      </Link>
 
-      <div>
+      <div className={styles.links}>
         {links.map((link) => (
-          <Link key={link.id} href={link.url}>
+          <Link key={link.id} href={link.url} className={styles.link}>
             {link.title}
           </Link>
         ))}
 
-        <button onClick={() => console.log("logget out")}>Logout</button>
+        <button
+          onClick={() => console.log("logget out")}
+          className={styles.logout}
+        >
+          Logout
+        </button>
       </div>
     </div>
   );
