e95a2e4ee87dc3b6a754cf0a6dcae0f5756816e0
Alek23k <alemank23k@gmail.com>
2023-06-11 11:50:24 -0300

    last commit

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -40,7 +40,7 @@ const Dashboard = () => {
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
   const { data, mutate, error, isLoading } = useSWR(
-    `/api/posts?username=${session?.data?.user.name}`,
+    `https://blog-next-7zhlpnpcg-alek30k.vercel.app/api/posts?username=${session?.data?.user.name}`,
     fetcher
   );
 
