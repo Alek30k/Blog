7ff1833bd9184e47c01305f5003aaa438c57ff2d
Alek23k <alemank23k@gmail.com>
2023-06-11 16:17:33 -0300

    last commit

--- next.config.js
+++ next.config.js
@@ -3,29 +3,6 @@ const nextConfig = {
   images: {
     domains: ["images.pexels.com"],
   },
-};ss
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: "/api/:path*", // Ruta de tu API
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*", // o el origen específico que deseas permitir
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS", // Métodos permitidos
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "X-Requested-With, Authorization, Content-Type, Accept", // Encabezados permitidos
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
