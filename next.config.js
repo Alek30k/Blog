f3b5d44ef94b843c639a426b347cc5306abef185
Alek23k <alemank23k@gmail.com>
2023-06-11 16:36:41 -0300

    last commit

--- next.config.js
+++ next.config.js
@@ -3,18 +3,13 @@ const nextConfig = {
   images: {
     domains: ["images.pexels.com"],
   },
-};
-module.exports = {
   async headers() {
     return [
       {
         source: "/path/(.*)",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://blog-next-7ngvuu396-alek30k.vercel.app/",
-          }, // Change this to specific domain for better security
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Change this to specific domain for better security
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
